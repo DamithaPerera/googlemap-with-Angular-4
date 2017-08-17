f51643502efd2e8080d613210281c27efddc99f1
damithap <damithap@ellipsis.lk>
2017-08-17 18:39:52 +0530

    all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,100 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  //zoom level
+  zoom: number = 10;
+
+  //start position
+  lat: number = 7.8731;
+  lng: number = 80.7718;
+
+  //values
+  markerName:string;
+  markerLat:string;
+  markerLng:string;
+  markerDraggable:string;
+
+ // marks
+  markers: marker[]= [
+    {
+      name:'company one',
+      lat:6.8677256,
+      lng: 79.8765835,
+      draggable: true
+    },
+    {
+      name:'company two',
+      lat: 6.8728282,
+      lng: 79.8914346,
+      draggable: true
+    },
+    {
+      name:'company three',
+      lat: 6.8565121,
+      lng: 79.8729812,
+      draggable: true
+    }
+  ];
+
+  constructor(){
+
+  }
+
+  clickedMarker(marker:marker, index:number){
+    console.log('Clicked Marker:'+marker.name+' at index'+index);
+  }
+
+  mapClicked($event:any){
+    console.log("map clicked");
+    var newMarker = {
+      name:'Untitled',
+      lat:$event.coords.lat,
+      lng:$event.coords.lng,
+      draggable: false
+    }
+
+    this.markers.push(newMarker);
+  }
+
+  markerDragEnd(marker:any, $event:any){
+    console.log('dragEnd',marker,$event);
+
+    var upMarker= {
+      name:marker.name,
+      lat:parseFloat(marker.lat),
+      lng:parseFloat(marker.lng),
+      draggable:false
+    }
+
+    var newLat = $event.coords.lat;
+    var newLng = $event.coords.lng;
+  }
+
+  addMarker(){
+    console.log('adding marker');
+    if(this.markerDraggable=='yes'){
+        var isDraggable=true;
+    }else {
+      var  isDraggable=false;
+    }
+
+    var newMarker = {
+      name:this.markerName,
+      lat:parseFloat(this.markerLat),
+      lng:parseFloat(this.markerLng),
+      draggable:isDraggable
+    }
+
+    this.markers.push(newMarker);
+
+  }
+}
+//marker type
+
+interface marker{
+  name?:string;
+  lat:number;
+  lng:number;
+  draggable:boolean;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyADYQrvino4xhHOPI7UD4FpvQXILysY6Q8'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
